b850d734f74459dc3c8180bd12a4800af01030d8
vikas00700 <v18663748@gmail.com>
2024-08-01 15:29:11 +0530

    Add files via upload

---
+++ set.js
@@ -0,0 +1,13 @@
+let num=new Set("bookkeper");
+console.log(num);
+let num1=new Set();
+num.add(3),
+num.add(2),
+num.add(3),
+num.add("naveen"),
+num.add("chirag")
+console.log(num1);
+
+console.log(num.has(2));
+console.log(num.has(3));
+console.log(num.has(5));
