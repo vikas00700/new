3e6e5189336601b5270644a10156b9aefb617c6e
vikas00700 <v18663748@gmail.com>
2024-08-01 15:29:56 +0530

    Add files via upload

---
+++ set2.js
@@ -0,0 +1,13 @@
+let num=new Set("sagar");
+console.log(num);
+let num1=new Set();
+num.add(300),
+num.add(200),
+num.add(600),
+num.add("vikas"),
+num.add("shashi")
+console.log(num1);
+
+console.log(num.has(200));
+console.log(num.has(606));
+console.log(num.has(576));
